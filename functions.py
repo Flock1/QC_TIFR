574d009a7eb00f2e2169c4a70f311dee0008c061
sarvagya1991 <sarvagya.gupta18@gmail.com>
2019-12-08 16:10:50 +0530

    the fucntions

---
+++ functions.py
@@ -0,0 +1,91 @@
+from qiskit.quantum_info import random_state
+import numpy as np
+import math
+import decimal
+
+def state_norm(state):
+    norm = 0.
+    for i in range(state.shape[0]):
+        norm += abs(state[i])**2
+    norm = math.sqrt(norm)
+    state = state/(norm)
+    return state
+
+
+def bit_flip_Y(x):
+    matrix = np.array([[0, -1j],[1j,0]])
+    x = np.squeeze(x)
+    probs = np.matmul(matrix,x)
+#     prob_1 = probs[0]
+#     prob_2 = probs[1]
+#     return prob_1, prob_2
+    return probs
+
+def measurement(x,y):
+    p1 = abs(x)**2
+    p2 = abs(y)**2
+    diff = 1 - (p1+p2)
+    p1 = p1+diff
+    return np.random.choice(2, 1, p=[p1,p2])
+
+def bit_flip_X(x):
+    matrix = np.array([[0, 1],[1,0]])
+    x = np.squeeze(x)
+    probs = np.matmul(matrix,x)
+#     prob_1 = probs[0]
+#     prob_2 = probs[1]
+#     return prob_1, prob_2
+    return probs
+
+def hadamard_X(x):
+    matrix = np.array([[1, 1],[1,-1]])
+    x = np.squeeze(x)
+    matrix = matrix/math.sqrt(2)
+#     state = np.array([x,y])
+    probs = np.matmul(matrix,x)
+#     prob_1 = probs[0]
+#     prob_2 = probs[1]
+#     return prob_1, prob_2
+    return probs
+
+def hadamard_Y(x):
+    matrix = np.array([[1, -1j],[1j,-1]])
+    x = np.squeeze(x)
+    matrix = matrix/math.sqrt(2)
+#     state = np.array([x,y])
+    probs = np.matmul(matrix,x)
+#     prob_1 = probs[0]
+#     prob_2 = probs[1]
+#     return prob_1, prob_2
+    return probs
+
+def nothing(x):
+    return x
+
+
+def reward(error_t, error_t1):
+    if(error_t> error_t1):
+        reward = 1
+    elif(error_t==error_t1):
+        reward = 0
+    elif(error_t<error_t1):
+        reward = -1
+    return reward
+
+
+def projection(state, final_state):
+#     e = (abs(state)**2 - abs(final_state)**2)
+    e = np.dot(state,final_state)
+    e = abs(e)
+    e = e**2
+    e = np.float32(np.squeeze(e))
+    return e
+
+
+def discounted_reward(r, gamma):
+    discounted_r = np.zeros_like(r)
+    running_add = 0
+    for i in reversed(range(0, r.size)):
+        running_add = running_add*gamma + r[i]
+        discounted_r[i] = running_add
+    return discounted_r
